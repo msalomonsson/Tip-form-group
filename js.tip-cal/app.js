fa4110228326dc67ca2d07371fc2ab07709fdd8c
msalomonsson <69800375+msalomonsson@users.noreply.github.com>
2020-11-20 14:06:44 +0100

    Add files via upload

--- js.tip-cal/app.js
+++ js.tip-cal/app.js
@@ -2,52 +2,63 @@
 class Ui{
     
     constructor(){
-        this.costBill = document.querySelector('#input-cost');
-        this.peopleBill = document.querySelector('#input-people');
         this.form = document.querySelector('.card-body');
         this.btn = document.querySelector('.btn');
-        this.serviceTest = document.querySelector('.service');
-        this.tipAmountP = document.querySelector('.tipAmount');
-        this.totalAmountP = document.querySelector('.totalAmount')
-        this.eachAmountP = document.querySelector('.eachAmount')
-        this.loading = document.querySelector('.loading')
+        this.tipAmountH2 = document.querySelector('.tipAmount');
+        this.totalAmountH2 = document.querySelector('.totalAmount');
+        this.eachAmountH2 = document.querySelector('.eachAmount');
+        this.loading = document.querySelector('.loading');
+        this.form.addEventListener('submit', this.renderToDom.bind(this));
     }
 
     renderToDom(e){
         e.preventDefault();
     
-        ui.loading.style.display = 'flex';
+        this.loading.style.display = 'flex';
     
         setTimeout(function(){
             ui.loading.style.display = 'none';
     
             const results = calc.calc();
     
-            ui.tipAmountP.innerText = `Total tip is $ ${results[0]}`;
-            ui.totalAmountP.innerText = `The total amount is $ ${results[1]}`;
-            ui.eachAmountP.innerText = `Each person owes $ ${results[2]}`
+            ui.tipAmountH2.innerText = `Total tip is $ ${results[0]}`;
+            ui.totalAmountH2.innerText = `The total amount is $ ${results[1]}`;
+            ui.eachAmountH2.innerText = `Each person owes $ ${results[2]}`
+
+            calc.costBill.value = '';
+            calc.peopleBill.value = '';
+            calc.serviceTest.value = 0;
         },2000)
+
+        setTimeout(function(){
+            ui.tipAmountH2.innerText = '';
+            ui.totalAmountH2.innerText = '';
+            ui.eachAmountH2.innerText = '';
+        },10000)
     } 
 }
 
-const ui = new Ui();
-ui.form.addEventListener('submit', ui.renderToDom);
-
-
 class Logic{
-    
+    constructor(){
+        this.costBill = document.querySelector('#input-cost');
+        this.peopleBill = document.querySelector('#input-people');
+        this.serviceTest = document.querySelector('.service');
+    }
     calc(){
-        let billCost = ui.costBill.value;
-        let billPeople = ui.peopleBill.value;
-        let service = (ui.serviceTest.value) / 100;
+        const billCost = this.costBill.value;
+        const billPeople = this.peopleBill.value;
+        const service = this.serviceTest.value / 100;
 
-        const tipAmount = (billCost) * service;
+        const tipAmount = (billCost) * (service);
         const totalAmount = Number (billCost) + Number (tipAmount); 
         const eachAmount = Number (totalAmount) / Number (billPeople);
 
         return [tipAmount,totalAmount,eachAmount]
     }
 }
+
+
+const ui = new Ui();
 
 const calc = new Logic();
 
